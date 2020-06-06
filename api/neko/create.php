768d7fbe82ba0363fb52a1836ffee38760986fc6
RDumancic <rdumancic@etfos.hr>
2020-06-06 14:32:39 +0200

    minore errore

--- api/neko/create.php
+++ api/neko/create.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
 require_once '../config/database.php';
-require_once '../objects/product.php';
+require_once '../objects/neko.php';
 require_once "../../config.php";
   
 $database = new Database();

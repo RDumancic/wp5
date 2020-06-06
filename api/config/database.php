e3dd139c83d01c74969eeacf1f6a3a16b8c13c2c
RDumancic <rdumancic@etfos.hr>
2020-06-06 14:00:10 +0200

    db

--- api/config/database.php
+++ api/config/database.php
@@ -2,10 +2,10 @@
 class Database{
   
     // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "api_db";
-    private $username = "root";
-    private $password = "";
+    private $host = "eu-cdbr-west-03.cleardb.net";
+    private $db_name = "heroku_93a244c76cc2cbb";
+    private $username = "b2acfd233df17c";
+    private $password = "f152baec";
     public $conn;
   
     // get the database connection
